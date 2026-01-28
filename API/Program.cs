39bb665a38d75738553b2970e940635cda85c3d3
cp-22rel <reto.lehmann@ceff.ch>
2026-01-28 14:13:18 +0100

    commit

--- API/Program.cs
+++ API/Program.cs
@@ -44,7 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//app.UseHttpsRedirection()f;
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
